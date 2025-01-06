6daa71a974e63ea49e44427a34150303b2bea638
RitikSingh555 <singh.ritik209@gmail.com>
2025-01-06 20:17:04 +0530

    add frontend deployed url

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,8 @@ class Config:
     # Define your allowed origins (could be an array of URLs)
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",  # React local dev server
-        "http://192.168.31.249:3000"  # Production frontend URL
+        "http://192.168.31.249:3000",
+        "https://home-price-prediction-three.vercel.app/"  # Production frontend URL
     ]
 
     # Optionally, you can also configure other CORS settings (e.g., allowed headers, methods, etc.)
